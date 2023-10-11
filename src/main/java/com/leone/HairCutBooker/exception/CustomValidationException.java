40c66424e2441474cc6c0dc48a9fbeabccd8ddb1
benia592 <benialeo592@gmail.com>
2023-10-11 10:49:38 +0200

    dynamic sorting on reservation service

---
+++ src/main/java/com/leone/HairCutBooker/exception/CustomValidationException.java
@@ -0,0 +1,8 @@
+package com.leone.HairCutBooker.exception;
+
+public class CustomValidationException extends RuntimeException{
+
+    public CustomValidationException(String message) {
+        super(message);
+    }
+}
